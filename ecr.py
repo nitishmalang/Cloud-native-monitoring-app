6e02b76e0fd1dca5e99f385ec4dde1d153fe871a
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-05-27 06:24:42 +0530

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -1,14 +1,26 @@
 import boto3
 
-# Replace 'your_region' with the actual region where your ECR repository exists
-ecr_client = boto3.client('ecr', region_name='ap-south-1')
+# Configure AWS credentials
+aws_access_key_id = 'cognizetech2@gmail.com'
+aws_secret_access_key = 'Guigui@420'
+aws_region = 'ap-south-1'  # Replace with your desired region
 
+# Create ECR client
+ecr_client = boto3.client('ecr',
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key,
+                          region_name=aws_region)
+
+# Define repository name
 repository_name = "my-cloud-native-repo"
 
+# Create ECR repository
 response = ecr_client.create_repository(repositoryName=repository_name)
 
+# Get repository URI
 repository_uri = response['repository']['repositoryUri']
 
 print(repository_uri)
+
 
 
